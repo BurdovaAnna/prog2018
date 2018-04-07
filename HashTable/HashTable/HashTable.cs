6e84dd2bc02cbf81992a351bb9461bc6c58934a9
BurdovaAnna <burdova.anya@yandex.ru>
2018-04-07 20:33:48 +0500

    Task4

---
+++ HashTable/HashTable/HashTable.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashTable
+{
+    public class HashTable
+    {
+        class HashEntry
+        {
+            public object Key { get; set; }
+            public object Value { get; set; }
+        }
+
+        List<List<HashEntry>> list;
+
+        /// <summary>
+        /// Конструктор контейнера
+        /// summary>
+        /// size">Размер хэ-таблицы
+        public HashTable(int size)
+        {
+            list = new List<List<HashEntry>>();
+            for (var i = 0; i < size; i++)
+                list.Add(new List<HashEntry>());
+        }
+
+        /// 
+        /// Метод складывающий пару ключь-значение в таблицу
+        /// 
+        /// key">
+        /// value">
+        public void PutPair(object key, object value)
+        {
+            var bucketNo = GetBucketNumber(key);
+            foreach (var item in list[bucketNo])
+            {
+                if (Equals(item.Key, key))
+                {
+                    item.Value = value;
+                    return;
+                }
+            }
+            list[bucketNo].Add(new HashEntry { Key = key, Value = value });
+        }
+        private int GetBucketNumber(object key)
+        {
+            return Math.Abs(key.GetHashCode()) % list.Count;
+        }
+
+        /// <summary>
+        /// Поиск значения по ключу
+        /// summary>
+        /// key">Ключь
+        /// <returns>Значение, null если ключ отсутствуетreturns>
+        public object GetValueByKey(object key)
+        {
+            var bucketNo = GetBucketNumber(key);
+            foreach (var item in list[bucketNo])
+            {
+                if (Equals(item.Key, key))
+                {
+                    return item.Value;
+                }
+            }
+            return null;
+        }
+    }
+}
+
---
+++ HashTable/HashTableTests/HashTableTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HashTableTests
+{
+    [TestClass]
+    public class HashTableTests
+    {
+        [TestMethod]
+        public void TestThreeElements()
+        {
+            //Добавление трёх элементов, поиск трёх элементов
+            var hashTable = new HashTable.HashTable(3);
+
+            hashTable.PutPair(1, "aaa");
+            hashTable.PutPair(2, "bbb");
+            hashTable.PutPair(3, "ccc");
+
+            Assert.AreEqual(hashTable.GetValueByKey(1), "aaa");
+            Assert.AreEqual(hashTable.GetValueByKey(2), "bbb");
+            Assert.AreEqual(hashTable.GetValueByKey(3), "ccc");
+        }
+
+        [TestMethod]
+        public void TestEqualElements()
+        {
+            //Добавление одного и того же ключа дважды с разными значениями сохраняет последнее добавленное значение
+            var hashTable = new HashTable.HashTable(2);
+
+            hashTable.PutPair(1, "aaa");
+            hashTable.PutPair(1, "bbb");
+
+            Assert.AreEqual(hashTable.GetValueByKey(1), "bbb");
+        }
+
+        [TestMethod]
+        public void Test10000Elements()
+        {
+            //Добавление 10000 элементов в структуру и поиск одного из них
+            var hashTable = new HashTable.HashTable(10000);
+
+            for (var i = 0; i < 10000; i++)
+                hashTable.PutPair(i, "a" + i);
+
+            Assert.AreEqual(hashTable.GetValueByKey(100), "a100");
+        }
+
+        [TestMethod]
+        public void TestNotAddedKeys()
+        {
+            //Добавление 10000 элементов в структуру и поиск 1000 недобавленных ключей, поиск которых должен вернуть null
+            var hashTable = new HashTable.HashTable(10000);
+
+            for (var i = 0; i < 10000; i++)
+                hashTable.PutPair(i, "a" + i);
+
+            for (var i = 10000; i < 11000; i++)
+                Assert.AreEqual(hashTable.GetValueByKey(i), null);
+        }
+    }
+}
